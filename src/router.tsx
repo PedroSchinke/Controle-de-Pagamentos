4bd38040675f3bf79851ab50367282fba022b0a0
Pedro Schinke <pedro.schinke@outlook.com>
2023-11-24 20:49:34 -0300

    added routes to new pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/defaultLayout'
 import { RegisterPage } from './pages/register page/RegisterPage'
+import { SearchPage } from './pages/search page/SearchPage'
+import { Dashboard } from './pages/dashboard/Dashboard'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cadastrar" element={<RegisterPage />} />
+        <Route path="/consultar" element={<SearchPage />} />
       </Route>
     </Routes>
   )
