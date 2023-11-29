324144e8b6174fb48b67eee495bd678ad91d71dd
Pedro Schinke <pedro.schinke@outlook.com>
2023-11-29 01:50:35 -0300

    detailed client page update

--- src/pages/detailed item pages/detailed client page/DetailedClient.tsx
+++ src/pages/detailed item pages/detailed client page/DetailedClient.tsx
@@ -7,9 +7,55 @@ import {
   ClientOptionButtons,
   UpdateClientButton,
 } from './styles'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { api } from '../../../services/api'
+import { formatDate } from '../../../services/format-date-service'
+import { ClientProps } from '../../../context/clientsContext'
 
 export function DetailedClient() {
+  const { id } = useParams()
+
+  const [client, setClient] = useState<ClientProps | null>(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await api.get(`/clientes/${id}`)
+
+        setClient(response.data)
+      } catch (error) {
+        console.error('Erro ao obter detalhes do cliente:', error)
+      }
+    }
+
+    getData()
+  }, [id])
+
+  if (!client) {
+    return <p>Carregando...</p>
+  }
+
+  const originalDates = [client.dataCadastro, client.dataAtualizacao]
+  const formattedDates = formatDate(originalDates)
+
+  const dataCadastro = formattedDates[0]
+  const dataAtualizacao = formattedDates[1]
+
+  const handleDeleteClient = async () => {
+    try {
+      const response = await api.delete(`/clientes/${id}`)
+
+      if (response.status === 200) {
+        console.log('Cliente deletado com sucesso!')
+      } else {
+        console.error('Erro ao deletar cliente. Status:', response.status)
+      }
+    } catch (error) {
+      console.error('Não foi possível deletar cliente:', error)
+    }
+  }
+
   return (
     <DetailedClientLayout>
       <DetailedClientContainer>
@@ -23,23 +69,23 @@ export function DetailedClient() {
         <DetailedClientInfos>
           <div>
             <span>Nome</span>
-            <h2>Fulano da Silva</h2>
+            <h2>{client.nome}</h2>
           </div>
           <div>
             <span>Email</span>
-            <h2>email@email.com.br</h2>
+            <h2>{client.email}</h2>
           </div>
           <div>
             <span>Telefone</span>
-            <h2>(51)983145592</h2>
+            <h2>{client.celular}</h2>
           </div>
           <div>
             <span>Cadastrado em</span>
-            <h2>25/11/2023 14:50</h2>
+            <h2>{dataCadastro}</h2>
           </div>
           <div>
             <span>Editado em</span>
-            <h2>25/11/2023 15:50</h2>
+            <h2>{dataAtualizacao}</h2>
           </div>
         </DetailedClientInfos>
         <ClientOptionButtons>
@@ -47,7 +93,7 @@ export function DetailedClient() {
             <Pencil />
             editar
           </UpdateClientButton>
-          <DeleteClientButton>
+          <DeleteClientButton onClick={handleDeleteClient}>
             <Trash />
             excluir
           </DeleteClientButton>
